fc45a55ae329a07f421802af876a23a6bdeebb41
oereo <dlstpgns0406@gmail.com>
2021-05-27 12:51:19 +0900

    refactor: fix url settings and query format

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -126,3 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'blog', 'static')
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
